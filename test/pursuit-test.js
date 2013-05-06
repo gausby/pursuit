f9ac344e5108a9495be55cbb109fc15bb9c267de
Martin Gausby <martin@gausby.dk>
2013-05-06 10:53:47 +0200

    It now compiles a query to source and is now able to check for equality

--- test/pursuit-test.js
+++ test/pursuit-test.js
@@ -10,6 +10,33 @@ var assert = buster.assertions.assert;
 var refute = buster.assertions.refute;
 
 buster.testCase('Pursuit', {
-    '// should compile a query language into a JavaScript function': function () {
+    'should compile a query language into a JavaScript function': function () {
+        assert.isFunction(pursuit({
+            test: { equals: 'test' }
+        }));
+    },
+
+    'should return a function that returns true if no query is given': function () {
+        assert.equals(pursuit({}).toString(), 'function anonymous(entry) {\nreturn true\n}');
+    },
+
+    'should make Arrays into OR statements': function () {
+        assert.equals(
+            pursuit([
+                [{ bar: { equals: 'baz' }},{ baz: { equals: 'foo' }}],
+                { foo: { equals: 'bar' }}
+            ]).toString(),
+            'function anonymous(entry) {\nreturn ((entry["bar"] === "baz")||(entry["baz"] === "foo"))||(entry["foo"] === "bar")\n}'
+        );
+    },
+
+    'should be able to check for equality': function () {
+        var query = pursuit({
+            foo: { equals: 'bar' }
+        });
+
+        assert.isTrue(query({'foo': 'bar'}));
+        refute.isTrue(query({'foo': 'baz'}));
     }
+
 });
