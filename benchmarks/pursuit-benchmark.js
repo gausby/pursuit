2067ec842622c3a67bb749b7ceb4e829008ab6ba
Martin Gausby <martin@gausby.dk>
2013-10-30 18:39:36 +0100

    Changed the benchmark test to use a static data set; The script that create the static data set is included. Also, the benchmark test now check for something a bit more complicated.

--- benchmarks/pursuit-benchmark.js
+++ benchmarks/pursuit-benchmark.js
@@ -2,30 +2,45 @@
 'use strict';
 
 var pursuit = require('../lib/pursuit'),
-    person = require('./random-person')
+    testSet = require('./data.json')
 ;
 
-var testSet = [];
-
-// perhaps the tests should be done with a static data set
-for (var i = 0; i < 100; i += 1) {
-    testSet.push(person());
-}
-
-var query = {
-    'name': {
-        'last': {
-            beginsWith: 'Ha',
-            endsWith: 'sen'
-        }
+var query = [
+    {
+        'name': {
+            'last': {
+                beginsWith: 'P',
+                endsWith: 'son'
+            }
+        },
+        'age': {
+            greaterThanOrEqualTo: 21,
+            lessThan: 68
+        },
+        'gender': {
+            equals: 'Female'
+        },
+        'occupation': { equals: 'Rehabilitation Services Director' }
+    },
+    {
+        'gender': 'Male',
+        'name': { first: {equals: 'Ian' } },
+        'age': { greaterThanOrEqualTo: 21, lessThan: 90},
+        'occupation': { contains: 'Business' }
     }
-};
+];
 
 var optimized = pursuit(query);
 var nonOptimized = pursuit.call({optimize: false}, query);
 
 module.exports = {
     name: 'Pursuit',
+    onComplete: function() {
+        console.log(
+            'Optimized found:', testSet.filter(optimized).length,
+            'Non-optimized found:', testSet.filter(nonOptimized).length
+        );
+    },
     tests: {
         'optimized': {
             fn: function() {
